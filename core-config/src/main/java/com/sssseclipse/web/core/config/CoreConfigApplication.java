8d1ed838349345019ba2057644fc617f975d990a
ssSsEclipse <hkgeclipse@gmail.com>
2018-12-27 23:49:55 +0800

    Change to read config from DB for core-config

--- core-config/src/main/java/com/sssseclipse/web/core/config/CoreConfigApplication.java
+++ core-config/src/main/java/com/sssseclipse/web/core/config/CoreConfigApplication.java
@@ -3,11 +3,11 @@ package com.sssseclipse.web.core.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.mongodb.EnableMongoConfigServer;
 
 @EnableDiscoveryClient
-@EnableConfigServer
 @SpringBootApplication
+@EnableMongoConfigServer
 public class CoreConfigApplication {
 
 	public static void main(String[] args) {
